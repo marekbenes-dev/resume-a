d7da6ad2375e55261d6eeb2dff0dad88d33b60a5
marek.benes <benes.marek11@gmail.com>
2025-10-09 14:52:25 +0200

    capacitator added

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.marekbenes.resume',
+  appName: 'resume-a',
+  webDir: 'dist/resume-a/browser'
+};
+
+export default config;
